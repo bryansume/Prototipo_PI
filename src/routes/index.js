1c4df8f5f4d07bb6373f3ad94a55e28873d2c40a
Bryan Suquillo <brsume@alumnatflorida.es>
2024-12-19 10:03:22 +0100

    creacion de las 5 rutas/ refactorización de las funciones con las flechas

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const router = Router();
 
 //Raiz
-router.get("/rutaGetConRouter", function (req, res) {
+router.get("/rutaGetConRouter", (req, res) => {
   console.log(
     "Hola en la consola node, desde una llamada en el servidor, la raiz"
   );
@@ -10,8 +10,27 @@ router.get("/rutaGetConRouter", function (req, res) {
 });
 
 //2ª ruta
-router.get("/ruta2", function (req, res) {
+router.get("/ruta2", (req, res) => {
+  console.log("Hola en la consola node, desde la segunda ruta");
   res.send("Hola desde la segunda ruta");
+});
+
+//3ª ruta
+router.get("/ruta3", (req, res) => {
+  console.log("Hola en la consola node, desde la tercera ruta");
+  res.send("Hola desde la tercera ruta");
+});
+
+//4ª ruta
+router.get("/ruta4", (req, res) => {
+  console.log("Hola en la consola node, desde la cuarta ruta");
+  res.send("Hola desde la cuarta ruta");
+});
+
+//5ª ruta
+router.get("/ruta5", (req, res) => {
+  console.log("Hola en la consola node, desde la quinta ruta");
+  res.send("Hola desde la quinta ruta");
 });
 
 module.exports = router;
