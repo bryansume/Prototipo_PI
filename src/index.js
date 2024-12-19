d3b85c21433e1e7b6d4af1a732e8542564485232
Bryan Suquillo <brsume@alumnatflorida.es>
2024-12-19 09:42:16 +0100

    añadir la llamada del módulo que hemos creado en archivo routes/index.js

--- src/index.js
+++ src/index.js
@@ -13,3 +13,6 @@ app.get("/rutaGet", function (req, res) {
 function leerPuerto() {
   console.log("Hola desde el puerto " + app.get("port"));
 }
+
+//Para que funcione esta 2º ruta es necesario añadir la llamada del módulo recién creado en el archivo inicial (PROTOTIPO/src/index.js) es decir este mismo archivo.
+app.use(require("./routes/index"));
