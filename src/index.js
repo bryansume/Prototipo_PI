7b66394d18dd78c37298149966ced25bcd612771
Bryan Suquillo <brsume@alumnatflorida.es>
2024-12-19 09:57:41 +0100

    Refactorizando/ añadiendo función flecha

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ const app = express();
 
 app.set("port", process.env.PORT || 8080);
 
-app.listen(app.get("port"), leerPuerto());
-
-function leerPuerto() {
+app.listen(app.get("port"), () => {
   console.log("Hola desde el puerto " + app.get("port"));
-}
+});
 
 //Para que funcione esta 2º ruta es necesario añadir la llamada del módulo recién creado en el archivo inicial (PROTOTIPO/src/index.js) es decir este mismo archivo.
 app.use(require("./routes/index"));
