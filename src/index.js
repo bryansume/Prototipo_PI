7670c2a5b51c71b6bbd5a7262760914f4fbe41f1
Bryan Suquillo <brsume@alumnatflorida.es>
2024-12-19 09:53:42 +0100

    paso la ruta a carpeta routes y cambio los mensajes que el usuario verá

--- src/index.js
+++ src/index.js
@@ -5,11 +5,6 @@ app.set("port", process.env.PORT || 8080);
 
 app.listen(app.get("port"), leerPuerto());
 
-app.get("/rutaGet", function (req, res) {
-  console.log("Hola en la consola node, desde una llamada en el servidor");
-  res.send("Hola desde una web llamada en el servidor");
-});
-
 function leerPuerto() {
   console.log("Hola desde el puerto " + app.get("port"));
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,15 @@ const router = Router();
 
 //Raiz
 router.get("/rutaGetConRouter", function (req, res) {
-  res.send("Hola desde una web llamada en el servidor utilizando routes");
+  console.log(
+    "Hola en la consola node, desde una llamada en el servidor, la raiz"
+  );
+  res.send("Bienvenido a Prototipo");
+});
+
+//2ª ruta
+router.get("/ruta2", function (req, res) {
+  res.send("Hola desde la segunda ruta");
 });
 
 module.exports = router;
