b7b8cab302690dc1e3e8a0502aeabe26f49aabed
Bryan Suquillo <brsume@alumnatflorida.es>
2025-02-08 17:16:02 +0100

    descomentado

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ const morgan = require("morgan");
 app.set("port", process.env.PORT || 8080);
 app.use(morgan("dev"));
 
-// app.listen(app.get("port"), () => {
-//   console.log("Hola desde el puerto " + app.get("port"));
-// });
+app.listen(app.get("port"), () => {
+  console.log("Hola desde el puerto " + app.get("port"));
+});
 
 //Para que funcione esta 2º ruta es necesario añadir la llamada del módulo recién creado en el archivo inicial (PROTOTIPO/src/index.js) es decir este mismo archivo.
 app.use(require("./routes/index"));
