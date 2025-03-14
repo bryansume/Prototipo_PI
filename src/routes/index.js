25815542c3f8ce09affa1ef28b9955af6631f010
Bryan Suquillo <brsume@alumnatflorida.es>
2025-03-14 11:23:55 +0100

    Modificación en las rutas(GET)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,24 +1,22 @@
 const { Router } = require("express");
 const router = Router();
 
-//1a Ruta con Get
-router.get("/rutaGetConRouter", (req, res) => {
-  console.log(
-    "Hola en la consola node, desde una llamada en el servidor, la raiz"
-  );
-  res.send("Bienvenido a Prototipo");
+//Ruta con Get
+router.get("/GET_Formulario", (req, res) => {
+  console.log(req.body);
+  res.send("Datos enviados del formulario con GET");
 });
 
-//Creación de la primera ruta con POST
-router.post("/POST1", (req, res) => {
-  console.log("Enviada primera petición al POST");
-  res.send("Es la primera ruta con POST!");
-});
+// //Creación de la primera ruta con POST
+// router.post("/POST1", (req, res) => {
+//   console.log("Enviada primera petición al POST");
+//   res.send("Es la primera ruta con POST!");
+// });
 
 //Ruta con body/request
 router.post("/POST_Formulario", (req, res) => {
   //Esto se verá en el Frontend
-  res.send("Se ha enviado los datos correctamente");
+  res.send("Datos enviados del formulario con POST");
   //Esto veremos nosotros en el Backend-Terminal
   console.log(req.body);
 });
